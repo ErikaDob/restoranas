f74a5bfc8840c81cfb5d4853b4314f4823c119e5
Your Name <you@example.com>
2017-05-03 21:35:37 +0300

    sukurta create forma

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,24 @@
+<form action="" method="post" enctype="multipart/form-data">
+	  <div class="form-group">
+	    <label>Title</label>
+	    <input class="form-control" value=" " name="title">
+	  </div>
+	  <div class="form-group">
+	    <label>Description</label>
+	    <textarea class="form-control" name="description">
+	  </textarea>
+	  <div class="form-group">
+	    <label>Price</label>
+	    <input class="form-control" value=" " name="price" placeholder="Length">
+	  </div>
+	  <div class="form-group">
+	    <label>Category</label>
+	    <input class="form-control" name="category"></input>
+	  </div>
+	  <div class="form-group">
+	    <label>Image</label>
+	    <input type="file" name="image">
+	  </div>
+	  <div class="form-group">
+	  <button type="submit" class="btn btn-primary">SUBMIT</button>
+	</form>
